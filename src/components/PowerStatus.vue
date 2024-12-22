4fb698fdf5e517c9b8b2b796c23c3cf588855495
lzt1008 <lzt1008@live.com>
2024-12-22 14:29:29 +0800

    feat: new design for `PowerStatus`

--- src/components/PowerStatus.vue
+++ src/components/PowerStatus.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import Badge from '@/components/ui/badge/Badge.vue'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import Progress from '@/components/ui/progress/Progress.vue'
 import { usePower } from '@/composables'
 import NumberFlow from '@number-flow/vue'
@@ -28,17 +27,41 @@ watch(focused, () => {
 <template>
   <div class="relative max-w-max basis-1/3 flex gap-4">
     <Card class="flex-1">
-      <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle class="text-base font-medium truncate mr-6">
-          Power Status
+      <CardHeader class="space-y-0 pb-2 gap-y-0">
+        <CardTitle class="flex items-center justify-between gap-2 text-base truncate">
+          <div class="mr-10 flex gap-2">
+            {{ power.isCharging ? 'Charging Power' : 'System Power' }}
+            <a
+              v-if="showOpenLink"
+              class="flex items-center gap-2 text-[10px] text-foreground/70 cursor-pointer"
+              @click="openApp"
+            >
+              <ExternalLink class="h-4 w-4" />
+            </a>
+          </div>
+          <div
+            class="rounded-md dark:bg-green-600 bg-green-500 px-2 py-1 text-xs truncate text-background/80 font-mono"
+          >
+            <span class="font-bold mr-1">{{ power.adapterDetails.watts }}W</span>
+            <span class="text-[10px]">({{ power.adapterDetails.voltage / 1000 }}V,{{
+              power.adapterDetails.amperage / 1000 }}A)</span>
+          </div>
         </CardTitle>
-        <div
-          class="flex items-center gap-2 rounded-md dark:bg-green-600 bg-green-500 px-2 py-1 text-xs truncate text-foreground/80"
-        >
+        <CardDescription class="text-[10px] font-mono">
           {{ power.adapterDetails.name }}
-        </div>
+        </CardDescription>
       </CardHeader>
       <CardContent class="space-y-4 mt-1">
+        <div class="flex flex-col gap-6">
+          <div class="flex gap-2">
+            <NumberFlow
+              class="text-4xl font-bold"
+              :format="{ maximumFractionDigits: 1, minimumFractionDigits: 1 }"
+              :value="power.isCharging ? power.systemIn : power.systemPower" suffix="w"
+            />
+          </div>
+        </div>
+
         <div class="flex flex-col gap-2">
           <div class="flex items-center justify-between">
             <div class="flex items-center gap-2">
@@ -50,36 +73,13 @@ watch(focused, () => {
               class="text-sm font-medium truncate"
               :class="power.isCharging ? 'text-green-500' : 'text-muted-foreground'"
             >
-              {{ power.isCharging ? 'Charging' : 'On Battery' }} • {{ power.timeRemaining }}
+              <span class="font-semibold">{{ power.isCharging ? 'Charging' : 'On Battery' }}</span>
+              • {{ power.timeRemaining }}
             </div>
           </div>
           <Progress :model-value="power.batteryLevel" />
         </div>
-
-        <div class="flex flex-col gap-6">
-          <div class="flex flex-col gap-2">
-            <div class="flex gap-2 text-sm font-medium text-muted-foreground">
-              <div>{{ power.isCharging ? `Charging Power` : 'System Power' }}</div>
-              <Badge v-if="power.isCharging" variant="secondary" class="font-normal -translate-y-[1px]">
-                {{ power.adapterDetails.watts }}W ({{ power.adapterDetails.voltage / 1000 }}V {{
-                  power.adapterDetails.amperage / 1000 }}A)
-              </Badge>
-            </div>
-            <!-- <div class="text-4xl font-bold transition-all">
-              {{ (power.isCharging ? power.systemIn : power.systemPower).toFixed(1) }}W
-            </div> -->
-            <NumberFlow class="text-4xl font-bold" :value="power.isCharging ? power.systemIn - power.systemIn % 0.1 : power.systemPower" suffix="w" />
-          </div>
-        </div>
       </CardContent>
     </Card>
-    <a
-      v-if="showOpenLink"
-      class="absolute flex items-center gap-2 text-xs text-foreground/70 bottom-4 right-4 cursor-pointer"
-      @click="openApp"
-    >
-      <ExternalLink class="h-4 w-4" />
-      Open App
-    </a>
   </div>
 </template>
