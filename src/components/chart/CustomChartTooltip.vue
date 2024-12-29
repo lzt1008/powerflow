ee0ca029de493fb2afdd3697fca76b89dd038b07
lzt1008 <lzt1008@live.com>
2024-12-30 00:01:55 +0800

    chore: tooltip

--- src/components/chart/CustomChartTooltip.vue
+++ src/components/chart/CustomChartTooltip.vue
@@ -16,15 +16,15 @@ const formatter = new Intl.NumberFormat('en-US', {
 
 <template>
   <Card class="text-sm">
-    <CardHeader v-if="title" class="p-3 border-b">
+    <!-- <CardHeader v-if="title" class="p-3 mb-0">
       <CardTitle>
         {{ title }}
       </CardTitle>
-    </CardHeader>
-    <CardContent class="p-3 min-w-[180px] flex flex-col gap-1">
+    </CardHeader> -->
+    <CardContent class="p-3 min-w-[100px] flex flex-col gap-2">
       <div v-for="(item, key) in data.filter(i => i.name)" :key="key" class="flex justify-between">
         <div class="flex items-center">
-          <span class="w-2.5 h-2.5 mr-2">
+          <span class="size-2.5 mr-2">
             <svg width="100%" height="100%" viewBox="0 0 30 30">
               <path
                 d=" M 15 15 m -14, 0 a 14,14 0 1,1 28,0 a 14,14 0 1,1 -28,0"
@@ -34,9 +34,9 @@ const formatter = new Intl.NumberFormat('en-US', {
               />
             </svg>
           </span>
-          <span>{{ item.name }}</span>
+          <span class="text-xs text-muted-foreground">{{ item.name }}</span>
         </div>
-        <span class="font-semibold ml-4">{{ typeof item.value === 'number' ? formatter.format(item.value) : item.value }}w</span>
+        <span class="text-xs font-semibold ml-4 font-mono">{{ typeof item.value === 'number' ? formatter.format(item.value) : item.value }}w</span>
       </div>
     </CardContent>
   </Card>
