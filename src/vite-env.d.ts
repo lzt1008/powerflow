1b96f0df5a3a0df08f80769dc92d7095fd42e1f8
lzt1008 <lzt1008@live.com>
2024-12-29 09:21:13 +0800

    chore: remove DefineComponent in dts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,4 @@
 /// <reference types="vite/client" />
 
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
-
 declare const __BUILD_DATE__: string
 declare const __COMMIT_HASH__: string
