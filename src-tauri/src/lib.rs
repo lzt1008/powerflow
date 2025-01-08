5395fe9c6352f49b1cc4b5637a78b4aeff64fe23
lzt1008 <lzt1008@live.com>
2025-01-09 01:00:16 +0800

    chore: add dialog plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -162,6 +162,7 @@ pub fn create_specta() -> tauri_specta::Builder {
 pub fn run() {
     let specta = create_specta();
     let app = tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(
             tauri_plugin_log::Builder::new()
