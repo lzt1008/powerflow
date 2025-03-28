64c90475e00e540015038a4fd6e19e9925907669
lzt1008 <lzt1008@live.com>
2025-03-28 13:57:39 +0800

    chore(ci): configure bumpp

---
+++ bump.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'bumpp'
+
+export default defineConfig({
+  files: [
+    'package.json',
+    'src-tauri/tauri.conf.json',
+    'src-tauri/Cargo.toml',
+    'crates/tpower/Cargo.toml',
+  ],
+  sign: true,
+})
